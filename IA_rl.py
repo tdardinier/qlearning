97874cd165e97a1993e6a5137b3b32334d406526
fandejuni <t.dardinier@gmail.com>
2018-03-05 23:59:27 +0100

    Cool snake

--- IA_rl.py
+++ IA_rl.py
@@ -2,6 +2,7 @@ from numpy import *
 import random as rd
 from pygame.locals import *
 import pygame
+import time
 
 MOVES = [
     (1, 0),
@@ -10,16 +11,20 @@ MOVES = [
     (0, 1)
 ]
 
-vision_max = 2
-n_sensors = 8 # We don't use the other snakes first
+vision_walls_max = 2
+odorat_candies_max = 8
+odorat_snake_max = 10
+
+sensors = [vision_walls_max for _ in range(4)] + [odorat_candies_max for _ in range(4)]
+n_sensors = len(sensors)
 n_actions = 4
 
 epsilon = 0.1
-tau = 0.5
-alpha = 0.1
-gamma = 0.99
+tau = 0.1
+alpha = 0.2
+gamma = 0.9
 actions = range(n_actions)
-n_states = vision_max ** n_sensors
+n_states = (vision_walls_max ** 4) * (odorat_candies_max ** 4)
 reward_dead = -10
 
 use_epsilon_greedy = False # Softmax otherwise
@@ -30,10 +35,13 @@ class IA():
     def __init__(self, agent_id, save = None):
 
         self.prev_s = None
-        self.prev_a = None
+        self.prev_a = 0
+        self.last_x = 0
+        self.last_y = 0
         self.id = agent_id
+        self.current_input = None
 
-        self.q = ones((n_states, n_actions))
+        self.q = ones((n_states, n_actions)) * 0.5
         if not (save is None):
             self.q = save
 
@@ -43,25 +51,42 @@ class IA():
     def numberize_state(self, s):
         r = 0
         m = 1
-        for x in s:
+        for i in range(n_sensors):
+            x = s[i]
             r += (x - 1) * m
-            m *= vision_max
+            m *= sensors[i]
         return r
 
+    def distance_to_candy(self, x, y, M):
+        d = odorat_candies_max
+        for (a, b) in M.candies:
+            d = min(d, abs(x - a) + abs(y - b))
+        return d
+
     def convert_input(self, M):
+
         head = M.agents[self.id].head()
+
         x = head[0]
         y = head[1]
 
-        walls = [vision_max for _ in range(4)]
-        snakes = [vision_max for _ in range(4)]
-        candies = [vision_max for _ in range(4)]
+        self.last_x = x
+        self.last_y = y
+
+        walls = [vision_walls_max for _ in range(4)]
+        snakes = [odorat_snake_max for _ in range(4)]
+
+        odorat_candies = []
+        odorat_candies.append(self.distance_to_candy(x + 1, y, M))
+        odorat_candies.append(self.distance_to_candy(x, y - 1, M))
+        odorat_candies.append(self.distance_to_candy(x - 1, y, M))
+        odorat_candies.append(self.distance_to_candy(x, y + 1, M))
 
         for i in range(len(MOVES)):
             (xx, yy) = MOVES[i]
 
             # Walls
-            dist_wall = vision_max
+            dist_wall = vision_walls_max
             if xx == 1:
                 dist_wall = M.gridsize - x
             elif xx == -1:
@@ -72,28 +97,10 @@ class IA():
                 dist_wall = y + 1
             else:
                 print("ERROR: shouldn't happen")
-            walls[i] = min(dist_wall, vision_max)
-
-            # Candies
-            dist_candy = M.gridsize
-            for (a, b) in M.candies:
-                if xx == 1:
-                    if y == b and x < a:
-                        dist_candy = min(dist_candy, a - x)
-                elif xx == -1:
-                    if y == b and x > a:
-                        dist_candy = min(dist_candy, x - a)
-                elif yy == 1:
-                    if x == a and y < b:
-                        dist_candy = min(dist_candy, b - y)
-                elif yy == -1:
-                    if x == a and y > b:
-                        dist_candy = min(dist_candy, y - b)
-                else:
-                    print("ERROR: shouldn't happen")
-            candies[i] = min(vision_max, dist_candy)
+            walls[i] = min(dist_wall, vision_walls_max)
 
             # Snakes
+            # TODO
             dist_snake = M.gridsize
             for agent in M.agents:
                 for (a, b) in agent.pos:
@@ -111,13 +118,13 @@ class IA():
                             dist_snake = min(dist_snake, y - b)
                     else:
                         print("ERROR: shouldn't happen")
-            snakes[i] = min(vision_max, dist_snake)
+            snakes[i] = min(odorat_snake_max, dist_snake)
 
-        print("Walls", walls)
-        print("Candies", candies)
-        print("Snakes", snakes)
+        #print("Walls", walls)
+        #print("Candies", candies)
+        #print("Snakes", snakes)
         #return walls + candies + snakes
-        return walls + candies
+        return walls + odorat_candies
 
     def proba_softmax(self, l):
         p = [exp(q_value / tau) for q_value in l]
@@ -140,10 +147,24 @@ class IA():
                 somme += p[i]
             return i
 
+    def print_choix(self, s):
+        spaces = " " * 4
+        l = ["%.2f" % x for x in self.q[s]]
+        print("")
+        print((self.last_x, self.last_y))
+        print(self.current_input)
+        print("Espace de choix")
+        print(spaces + l[1])
+        print(l[2] + spaces + l[0])
+        print(spaces + l[3])
+        print(l)
+        print("")
+
     def act(self, M, reward):
-        s = self.numberize_state(self.convert_input(M))
+        self.current_input = self.convert_input(M)
+        s = self.numberize_state(self.current_input)
         a = self.choose_action(s)
-        if not (self.prev_a is None):
+        if not (self.prev_s is None):
             if use_sarsa:
                 self.q[self.prev_s][self.prev_a] = (1 - alpha) * self.q[self.prev_s][self.prev_a] + alpha * (reward + gamma * self.q[s][a])
             else:
--- main_rl.py
+++ main_rl.py
@@ -12,53 +12,61 @@ nagents = 1
 IA = [IA_random.IA(i) for i in range(nagents)]
 #IA[1] = IA_keyboard.IA(1)
 IA[0] = IA_rl.IA(0)
-n_candies = 50
+
+n_candies = 5
+gridsize = 20
 
 step = 0
 
-M = Map(nagents=nagents, ncandies=n_candies, gridsize=40)
+M = Map(nagents=nagents, ncandies=n_candies, gridsize=gridsize)
 e = Render(M, spacing=20)
 
 reward = 0
+step_by_step = False
+
+def press():
+    pygame.event.pump()
+    return pygame.key.get_pressed()
 
 while True:
 
     while True:
+
+        if step_by_step:
+            while True:
+                keys = press()
+                if keys[K_RIGHT]:
+                    time.sleep(0.1)
+                    break
+                if keys[K_UP]:
+                    step_by_step = False
+                    break
         index = 0
         print(step)
         e.render()
-
-        for i in range(nagents):
-            reward = 0
-
-            if M.agents[i].has_eaten:
-                reward = 2
-                print("WOAOFRJFHDCBUDRHFLGHVRDKFDFHXGKJFHCK")
-                print("WOAOFRJFHDCBUDRHFLGHVRDKFDFHXGKJFHCK")
-                print("WOAOFRJFHDCBUDRHFLGHVRDKFDFHXGKJFHCK")
-                print("WOAOFRJFHDCBUDRHFLGHVRDKFDFHXGKJFHCK")
-                print("WOAOFRJFHDCBUDRHFLGHVRDKFDFHXGKJFHCK")
-                print("WOAOFRJFHDCBUDRHFLGHVRDKFDFHXGKJFHCK")
-                print("WOAOFRJFHDCBUDRHFLGHVRDKFDFHXGKJFHCK")
-                print("WOAOFRJFHDCBUDRHFLGHVRDKFDFHXGKJFHCK")
-                print("WOAOFRJFHDCBUDRHFLGHVRDKFDFHXGKJFHCK")
-                print("WOAOFRJFHDCBUDRHFLGHVRDKFDFHXGKJFHCK")
-                print("WOAOFRJFHDCBUDRHFLGHVRDKFDFHXGKJFHCK")
-                print("WOAOFRJFHDCBUDRHFLGHVRDKFDFHXGKJFHCK")
-                print("WOAOFRJFHDCBUDRHFLGHVRDKFDFHXGKJFHCK")
-
-            agent = IA[i]
-            M.agents[i].nextAction(agent.act(M, reward))
 
         if len(M.activeAgents) == 0:
             q = IA[0].dead()
             break
 
+        for i in range(nagents):
+            reward = 0
+
+            if M.agents[i].has_eaten:
+                reward = 1
+
+            agent = IA[i]
+            a = agent.act(M, reward)
+            print("ACTION ", a)
+            M.agents[i].nextAction(a)
+        keys = press()
+        if keys[K_DOWN]:
+            step_by_step = True
 
         step += 1
         r, done = M.step()
         #time.sleep(0.001)
 
-    M = Map(nagents=nagents, ncandies=n_candies, gridsize=40)
+    M = Map(nagents=nagents, ncandies=n_candies, gridsize=gridsize)
     e = Render(M, spacing=20)
     IA[0] = IA_rl.IA(0, q)
